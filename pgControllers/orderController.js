119d1985f413110a23237eb80d321826cf21ca46
Datsunco <danya200375d@gmail.com>
2023-09-14 15:58:38 +0300

    CorsFixtest

--- pgControllers/orderController.js
+++ pgControllers/orderController.js
@@ -26,13 +26,14 @@ const orderController = {
     getById: async(req, res) => {
         try {
             const {id} = req.query
+            
 
             const { rows } = await postgre.query("select * from orders where id = $1", [id])
 
             if (rows[0]) {
                 return res.json({msg: "OK", data: rows})
             }
-
+            
             res.status(404).json({msg: "not found"})
         } catch (error) {
             res.json({msg: error.msg})
@@ -41,6 +42,11 @@ const orderController = {
     create: async(req, res) => {
         try {
             const {userid, lon, lat, address, radius} = req.body
+
+            res.writeHead(200, {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "X-Foo"
+             });
 
             const sql = 'INSERT INTO orders(userid, lon, lat, address, radius) VALUES($1, $2, $3, $4, $5) RETURNING *'
 
