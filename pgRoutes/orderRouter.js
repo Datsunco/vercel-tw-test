96eacd89f23eb8d48a113303e0ffe9bf61ca72c0
DatsunCo <danya200375d@gmail.com>
2023-10-23 17:04:12 +0300

    cors

--- pgRoutes/orderRouter.js
+++ pgRoutes/orderRouter.js
@@ -3,9 +3,9 @@ const orderController = require('../pgControllers/orderController')
 const authMiddleware = require('../middleware/headermiddleware')
 const router = new Router()
 
-router.post('/create', authMiddleware,orderController.create)
-router.get('/getAll', authMiddleware, orderController.getByUserId)
-router.get('/getOne', authMiddleware, orderController.getById)
-router.options('/create', authMiddleware, orderController.create)
+router.post('/create',orderController.create)
+router.get('/getAll', orderController.getByUserId)
+router.get('/getOne',  orderController.getById)
+router.options('/create',  orderController.create)
 
 module.exports  = router
