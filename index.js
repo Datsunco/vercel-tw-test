72864c2d30d5fae7e5a133a7d02c6e50f5e729fd
DatsunCo <danya200375d@gmail.com>
2023-10-23 16:43:36 +0300

    cors

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const POPT = process.env.PORT || 5000
 const app = express()
 
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://heroic-brioche-432533.netlify.app/'
+}))
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
